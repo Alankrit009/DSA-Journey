35eccc55f68b02cc74cb16f82abae019cda7d6f7
Alankrit009 <alankrittainguriya@gmail.com>
2026-06-10 17:29:10 +0530

    Day 1

---
+++ day1.cpp
@@ -0,0 +1,100 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+cout<< "hello world";
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    int b=10;
+    cout<< a+b;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;    
+int main() {
+    int a=5;
+    int b=10;
+    cout << a*b;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    cout << s;
+    return 0;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int r=7;
+    cout<< 3.14*7*7;
+    return 0;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;    
+int main() {
+    int l=5;
+    int b=10;
+    cout << 2*(l+b);
+    return 0;
+*/
+/*
+#include <bits/stdc++.h> 
+using namespace std;
+int main (){
+    int a=5;
+    int b=10;
+    int c=a;
+    a=b;
+    b=c;
+    cout << a<< b;
+    return 0;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    int b=10;
+    int c = max(a,b);
+    cout << c;
+    return 0;
+}
+*/
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 4;
+    if (a%2==0){
+        cout << "even";
+    } else {
+        cout << "odd";
+    }
+    return 0;
+}
+*/
+/*  
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int c=100;
+    int f=(c*(1.8)+32);
+    cout << f;
+    return 0;
+}
+*/
